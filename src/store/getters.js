89d96b206449f19ac6781d43bc56a9af4b1f0ef2
Hunter Meyer <huntermeyer@live.com>
2019-08-20 01:31:13 -0400

    Add store

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+// https://vuex.vuejs.org/en/getters.html
+
+export default {
+  menu: state => { return state.menu },
+  getSettings: state => { return state.settings },
+  getPosts: state => { return state.posts },
+  getActivePost: state => { return state.activePost },
+  loading: state => { return state.status.loading },
+  getShareLinks: state => { return state.shareLinks },
+  getSocialSettings: state => { return state.socialSettings }
+}
