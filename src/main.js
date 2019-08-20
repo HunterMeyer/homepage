ab43b16f8ae8f0c29b6510111011dfa2008a2c76
Hunter Meyer <huntermeyer@live.com>
2019-08-20 00:29:53 -0400

    Initial setup of App and main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$siteName = process.env.VUE_APP_TITLE || 'Homepage'
+
+new Vue({
+  render: h => h(App),
+  created () {
+    console.log('App Title: ' + this.$siteName)
+  }
+}).$mount('#app')
