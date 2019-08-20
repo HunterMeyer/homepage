72498e56cdebba8c1bc3a24c39e4b06c8009e564
Hunter Meyer <huntermeyer@live.com>
2019-08-20 02:13:50 -0400

    Attempt at env config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // main.js
 import Vue from 'vue'
 import './plugins'
+
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store/index'
--- src/plugins/cosmic.js
+++ src/plugins/cosmic.js
@@ -1,9 +1,10 @@
+import config from './config'
 const Cosmic = require('cosmicjs')()
 
 const bucket = Cosmic.bucket({
-  slug: process.env.VUE_COSMIC_BUCKET,
-  read_key: process.env.VUE_COSMIC_READ_KEY,
-  write_key: process.env.VUE_COSMIC_WRITE_KEY
+  slug: config.VUE_COSMIC_BUCKET,
+  read_key: config.VUE_COSMIC_READ_KEY,
+  write_key: config.VUE_COSMIC_WRITE_KEY
 })
 
 module.exports = bucket
