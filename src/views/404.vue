958af67f8a7b13ec07cb62737f873c2a9db9ab23
Hunter Meyer <huntermeyer@live.com>
2019-08-20 01:31:30 -0400

    Add Home About and 404 views

---
+++ src/views/404.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <h1>404</h1>
+    <p>The page or file you're looking for doesn't exist.</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Error: 404 Not Found',
+    titleTemplate: '%s | '+ process.env.VUE_APP_TITLE
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div v-for="(post) in getPosts" :key="post._id">
+        <PostListItem :article="post"></PostListItem>
+      </div>
+      <app-post-dialog v-if="getActivePost" :article="getActivePost"></app-post-dialog>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import PostListItem from '@/components/Posts/PostListItem'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    PostListItem
+  },
+  computed: {
+    ...mapGetters([
+      'loading',
+      'getPosts',
+      'getActivePost'
+    ]),
+  },
+  metaInfo: {
+    title: 'Home',
+    titleTemplate: '%s | '+ process.env.VUE_APP_TITLE
+  }
+}
+</script>
