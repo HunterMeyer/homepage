46772f7fa5c4709a2209afeb84cadbc104b3c657
Hunter Meyer <huntermeyer@live.com>
2019-08-20 02:15:24 -0400

    Supposed to prefix with vue

--- src/plugins/cosmic.js
+++ src/plugins/cosmic.js
@@ -2,9 +2,9 @@ import config from './config'
 const Cosmic = require('cosmicjs')()
 
 const bucket = Cosmic.bucket({
-  slug: config.VUE_COSMIC_BUCKET,
-  read_key: config.VUE_COSMIC_READ_KEY,
-  write_key: config.VUE_COSMIC_WRITE_KEY
+  slug: config.COSMIC_BUCKET,
+  read_key: config.COSMIC_READ_KEY,
+  write_key: config.COSMIC_WRITE_KEY
 })
 
 module.exports = bucket
