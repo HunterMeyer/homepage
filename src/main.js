192bf6cbb912bacf4753ec641fd9369f8070a8be
Hunter Meyer <huntermeyer@live.com>
2019-08-20 23:01:57 -0400

    Rearrange

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// main.js
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins'
@@ -6,12 +5,10 @@ import router from '@/router'
 import store from '@/store/index'
 import '@/assets/css/tailwind.css'
 
-// Filters
-import DateFilter from '@/filters/Date'
-
 Vue.config.productionTip = false
 
-// Register the filters
+// Filters
+import DateFilter from '@/filters/Date'
 Vue.filter('date', DateFilter)
 
 new Vue({
