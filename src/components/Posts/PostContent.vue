6ee6106b3a08672324b8eadc2ebc4f2ae159fb40
Hunter Meyer <huntermeyer@live.com>
2019-08-20 23:02:10 -0400

    Create PostContent component and use it

---
+++ src/components/Posts/PostContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ post.fields.title }}</h1>
+    <div v-html="content"></div>
+  </div>
+</template>
+
+<script>
+import { BLOCKS } from '@contentful/rich-text-types'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+
+const renderOptions = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+      let { file, title } = node.data.target.fields
+      return `<img src=${file.url} title=${title} />`
+    }
+  }
+}
+
+export default {
+  name: 'PostContent',
+  props: {
+    post: {
+      type: Object,
+      require: true
+    }
+  },
+  computed: {
+    content() {
+      return documentToHtmlString(this.post.fields.content, renderOptions)
+    }
+  }
+}
+
+</script>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -3,27 +3,18 @@
     Loading...
   </div>
   <div v-else>
-    <h1>{{ currentPost.fields.title }}</h1>
-    <div v-html="content"></div>
+    <PostContent :post="currentPost" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import { BLOCKS } from '@contentful/rich-text-types'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
-
-const renderOptions = {
-  renderNode: {
-    [BLOCKS.EMBEDDED_ASSET]: node => {
-      let { file, title } = node.data.target.fields
-      return `<img src=${file.url} title=${title} />`
-    }
-  }
-};
-
+import PostContent from '@/components/Posts/PostContent'
 export default {
   name: 'Post',
+  components: {
+    PostContent
+  },
   props: {
     slug: {
       type: String
@@ -33,14 +24,7 @@ export default {
     ...mapGetters([
       'currentPost',
       'isLoading'
-    ]),
-    content() {
-      if (this.currentPost) {
-        return documentToHtmlString(this.currentPost.fields.content, renderOptions)
-      } else {
-        return ''
-      }
-    }
+    ])
   },
   created () {
     this.$store.dispatch('getPostBySlug', this.slug)
