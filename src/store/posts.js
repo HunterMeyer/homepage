493df57436d00ffec50cb4f4943e98eea99a2b4c
Hunter Meyer <huntermeyer@live.com>
2019-08-20 22:09:18 -0400

    State for posts

---
+++ src/store/posts.js
@@ -0,0 +1,43 @@
+import contentful from '@/plugins/contentful'
+
+export const state = () => ({
+  posts: [],
+  currentPost: {},
+  isLoading: true
+})
+
+export const getters = {
+  posts: state => { return state.posts },
+  currentPost: state => { return state.currentPost },
+  isLoading: state => { return state.isLoading }
+}
+
+export const mutations = {
+  setCurrentPost(state, payload) {
+    state.currentPost = payload
+  },
+  setLoading(state, payload) {
+    state.isLoading = payload
+  },
+  setPosts(state, payload) {
+    state.posts = payload
+  }
+}
+export const actions = {
+  async getPostBySlug({commit}, slug) {
+    commit('setLoading', true);
+    const response = await contentful.getEntries({
+      content_type: 'post',
+      'fields.slug': slug
+    })
+    commit('setCurrentPost', response.items[0])
+    commit('setLoading', false)
+  },
+  async getPosts({commit}) {
+    commit('setLoading', true);
+    const response = await contentful.getEntries({
+      content_type: 'post'
+    })
+    commit('setPosts', response.items)
+  }
+}
