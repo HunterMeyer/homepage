d984c4d9a62c7c9853dc13788c82bb697687b60e
Hunter Meyer <huntermeyer@live.com>
2019-08-20 22:11:19 -0400

    Create PostCard and Post vue

--- src/components/Posts/PostCard.vue
+++ src/components/Posts/PostCard.vue
@@ -1,11 +1,13 @@
 <template lang="html">
-  <div @click="onClick">
-    <h2>{{ post.fields.title }}</h2>
+  <router-link :to="{ name: 'Post', params: { slug: post.fields.slug } }">
     <div>
-      <p v-if="post.fields.author">{{ post.sys.createdAt | date }} by {{ post.fields.author }}</p>
-      <p v-else>{{ post.sys.createdAt | date }}</p>
+      <h2>{{ post.fields.title }}</h2>
+      <div>
+        <p v-if="post.fields.author">{{ post.sys.createdAt | date }} by {{ post.fields.author }}</p>
+        <p v-else>{{ post.sys.createdAt | date }}</p>
+      </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
@@ -15,11 +17,6 @@ export default {
     post: {
       type: Object,
       required: true
-    }
-  },
-  methods: {
-    onClick () {
-      this.$router.push('/posts/' + this.article.slug)
     }
   }
 }
---
+++ src/views/Post.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="isLoading">
+    Loading...
+  </div>
+  <div v-else>
+    <h1>{{ currentPost.fields.title }}</h1>
+    <div v-html="content"></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { BLOCKS } from '@contentful/rich-text-types'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+
+const renderOptions = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+      let { file, title } = node.data.target.fields
+      return `<img src=${file.url} title=${title} />`
+    }
+  }
+};
+
+export default {
+  name: 'Post',
+  props: {
+    slug: {
+      type: String
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'currentPost',
+      'isLoading'
+    ]),
+    content() {
+      if (this.currentPost) {
+        return documentToHtmlString(this.currentPost.fields.content, renderOptions)
+      } else {
+        return ''
+      }
+    }
+  },
+  watch: {
+    currentPost: function (newValue) {
+      this.$route.meta.title = newValue.fields.title
+    }
+  },
+  created () {
+    this.$store.dispatch('getPostBySlug', this.slug)
+  },
+  metaInfo: {
+    titleTemplate: '%s | ' + process.env.VUE_APP_TITLE
+  }
+}
+</script>
