a7333dc5f72e2cfc8b8ff2d5f0d8cd104d04429f
Hunter Meyer <huntermeyer@live.com>
2019-08-20 22:08:31 -0400

    Remove old Cosmic store/plugins

--- src/plugins/cosmic.js
+++
@@ -1,7 +0,0 @@
-const Cosmic = require('cosmicjs')()
-const bucket = Cosmic.bucket({
-  slug: process.env.VUE_APP_COSMIC_BUCKET,
-  read_key: process.env.VUE_APP_COSMIC_READ_KEY,
-  write_key: process.env.VUE_APP_COSMIC_WRITE_KEY
-})
-module.exports = bucket
--- src/store/mutations.js
+++
@@ -1,7 +0,0 @@
-// https://vuex.vuejs.org/en/mutations.html
-
-export default {
-  setPosts(state, payload) {
-    state.posts = payload
-  }
-}
