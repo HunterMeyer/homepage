5134f1c97d5668c7c69bfe5688ca64c001b2c723
Hunter Meyer <huntermeyer@live.com>
2019-09-05 00:26:44 -0400

    WIP JrDevJobs.vue page

---
+++ src/views/projects/JrDevJobs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl text-orange-500">Jr.DevJobs</h1>
+    <h3 class="text-2xl text-gray-600">Jobs for Junior Developers</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JrDevJobs',
+  metaInfo: {
+    title: 'Jr.DevJobs Project',
+    titleTemplate: '%s | '+ process.env.VUE_APP_TITLE
+  }
+}
+</script>
