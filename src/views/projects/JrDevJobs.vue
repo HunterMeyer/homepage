875ae5eec37bca7f2922a648826d00bc857758be
Hunter Meyer <huntermeyer@live.com>
2019-09-10 00:43:56 -0400

    Styling hero

--- src/views/projects/JrDevJobs.vue
+++ src/views/projects/JrDevJobs.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="mx-auto">
-    <div class="flex">
-      <div class="w-full py-10 text-center">
-        <h1 class="text-4xl leading-none">Jr.DevJobs</h1>
-        <div class="text-sm text-gray-500 mt-3">Jobs for Junior Developers</div>
+    <section class="hero-bg mb-10 sm:mb-0">
+      <div class="flex flex-wrap mx-auto max-w-5xl px-4">
+        <div class="w-full sm:w-1/2 self-center pb-2 pt-10 sm:pt-2">
+          <div class="text-center text-white">
+            <p class="leading-none font-thin uppercase text-teal-200" style="letter-spacing:1rem;">Project</p>
+            <h1 class="text-6xl font-semibold leading-none">Jr.DevJobs</h1>
+            <h2 class="text-3xl font-thin leading-none">Jobs for Junior Developers</h2>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2 self-center -mb-32">
+          <div class="relative">
+            <img alt="Image placeholder" src="/images/iphone.png" class="mx-auto" style="max-width:90%; height:auto">
+          </div>
+        </div>
       </div>
-    </div>
+      <div style="fill: #f7fafc;">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 220" preserveAspectRatio="none" class="ie-shape-wave-3">
+          <path d="M918.34,99.41C388.23,343.6,47.11,117.12,0,87.54V220H1600V87.54C1378.72-76.71,1077.32,27.41,918.34,99.41Z"></path>
+        </svg>
+      </div>
+    </section>
     <div class="px-4 py-8 mx-auto max-w-5xl">
       <div class="flex flex-wrap">
         <div class="w-full md:w-2/3 px-2 mb-4 md:mb-0">
@@ -71,3 +86,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  .hero-bg {
+    background: linear-gradient(50deg, #63c3da, #81e6d9);
+  }
+</style>
