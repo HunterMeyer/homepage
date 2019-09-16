e04c8f9fb4889cc3e6c0c527fe64d2d74c027a80
Hunter Meyer <huntermeyer@live.com>
2019-09-15 22:14:13 -0400

    Add max-width to navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,15 +1,17 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-gray-700 px-6 py-4">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <span class="font-semibold text-xl tracking-tight">Hunter Meyer</span>
-    </div>
-    <div class="flex-grow flex items-center w-auto">
-      <div class="text-md flex-grow" role="menu">
-        <span v-for="(path, index) in paths" :key="index">
-          <router-link :to="path.to" :exact="path.exact" active-class="border-b-2 border-white" role="menuitem" class="inline-block text-teal-300 hover:text-white mr-4">
-            {{ path.title }}
-          </router-link>
-        </span>
+  <nav class="flex items-center justify-between flex-wrap bg-gray-700 py-4">
+    <div class="flex max-w-5xl w-full mx-auto px-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Hunter Meyer</span>
+      </div>
+      <div class="flex-grow flex items-center w-auto">
+        <div class="text-md flex-grow" role="menu">
+          <span v-for="(path, index) in paths" :key="index">
+            <router-link :to="path.to" :exact="path.exact" active-class="border-b-2 border-white" role="menuitem" class="inline-block text-teal-300 hover:text-white mr-4">
+              {{ path.title }}
+            </router-link>
+          </span>
+        </div>
       </div>
     </div>
   </nav>
