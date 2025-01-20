0b794e4d9252acd9689b6fb564b5b5c342512a78
Hunter Meyer <huntermeyer@live.com>
2025-01-19 19:53:26 -0500

    Add ReadlingList page, data wip

---
+++ src/components/Details/ReadingList.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseCard class="flex flex-col gap-6 text-black dark:text-white md:pt-20">
+    <section class="grid grid-cols-1 gap-8 md:grid-cols-2">
+      <PortfolioItem
+        v-for="(item, index) in items"
+        :key="index"
+        :item="item"
+      />
+    </section>
+  </BaseCard>
+</template>
+
+<script setup>
+import { readingListData } from "@/data/reading-list.js"
+import BaseCard from "../BaseCard.vue"
+import PortfolioItem from "./PortfolioItem.vue"
+
+const items = readingListData.filter((item) => item.visible)
+</script>
---
+++ src/data/reading-list.js
@@ -0,0 +1,77 @@
+export const readingListData = [
+  {
+    name: "Meyer Visions",
+    caption: "Funding the Future",
+    description: `Meyer Visions is a venture capital firm that invests early in
+      founders building fast-growing technology companies.`,
+    url: "https://www.meyervisions.com",
+    imageUrl: "/images/portfolio/meyer-visions.webp",
+    visible: true,
+  },
+  {
+    name: "Jr.DevJobs",
+    caption: "Jobs for Junior Developers",
+    description: `Find your first dev job! Featuring jobs and paid internships for
+      bootcamp grads, CS students & self taught programmers.`,
+    url: "https://www.jrdevjobs.com",
+    imageUrl: "/images/portfolio/jrdevjobs.webp",
+    visible: true,
+    css: {
+      "object-position": "center top",
+    },
+  },
+  {
+    name: "MonthlyStash",
+    caption: "Discreetly Delivering Delta-9",
+    description: `MonthlyStash is a subscription service that discreetly delivers
+      premium CBD and Delta-9 experiences and products to your doorstep every month.`,
+    url: "https://www.monthlystash.com",
+    imageUrl: "/images/portfolio/monthlystash.webp",
+    visible: true,
+  },
+  {
+    name: "Backspace",
+    caption: "Online Communities Made Easy",
+    description: `Backspace allows creators and influencers to connect, grow, and
+      monitize their online communities.`,
+    url: "https://www.backspacethat.com/",
+    imageUrl: "/images/portfolio/backspace.webp",
+    visible: true,
+    css: {
+      "object-position": "center top",
+    },
+  },
+  {
+    name: "Edge Guard Games",
+    caption: "Melee Gaming Tournament",
+    description: `Get your Game On, you Rascal! Edge Guard Games is a Super Smas Bros.
+      Melee Tournament`,
+    url: null,
+    imageUrl: "/images/portfolio/edge-guard.webp",
+    visible: true,
+    css: {
+      "object-position": "center top",
+    },
+  },
+  {
+    name: "ZenScapes",
+    caption: "Create your Landscape Oasis",
+    description: `A Complete, Creative Solution for your Landscape. Proudly Serving
+      North Fulton, Cherokee and Forsyth counties.`,
+    url: "http://www.zenscapesinc.com/",
+    imageUrl: "/images/portfolio/zenscapes.webp",
+    visible: true,
+    css: {
+      "object-position": "center top",
+    },
+  },
+  {
+    name: "Hangout.Ninja",
+    caption: "Stealthy Video and Text Chat",
+    description: `Easily create and join video-conference rooms. Includes real-time
+      chat, and anonymity`,
+    url: "https://github.com/HunterMeyer/hangout-ninja",
+    imageUrl: "/images/portfolio/hangout-ninja.webp",
+    visible: true,
+  },
+]
