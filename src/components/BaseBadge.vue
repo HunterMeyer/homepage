f0ca349ad17c45502da885948e0f50670ce4d52e
Hunter Meyer <huntermeyer@live.com>
2025-01-28 14:53:47 -0500

    Fix overflow of book genres

--- src/components/BaseBadge.vue
+++ src/components/BaseBadge.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="rounded-lg bg-black px-3 py-1 text-center text-xs font-light text-white dark:bg-my-black-800/50">
+  <div
+    class="rounded-lg bg-black px-3 py-1 text-center text-xs font-light text-white dark:bg-my-black-800/50"
+  >
     {{ title }}
   </div>
 </template>
 
 <script setup>
-const props = defineProps({
+defineProps({
   title: {
     type: String,
     required: true,
--- src/components/Details/ReadingListItem.vue
+++ src/components/Details/ReadingListItem.vue
@@ -5,15 +5,19 @@
         v-bind="linkAttrs"
         class="group col-span-1"
       >
-        <figure class="before:group-hover:bg-black/50 relative overflow-hidden rounded-2xl shadow-xl before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-transparent before:transition-all before:duration-200 before:ease-in-out before:content-['']">
-          <div class="absolute left-1/2 top-1/2 z-10 flex h-14 w-14 -translate-x-1/2 -translate-y-1/2 scale-75 items-center justify-center rounded-2xl bg-white text-yellow-500 opacity-0 shadow-lg transition-all duration-200 ease-in-out group-hover:scale-100 group-hover:opacity-100 dark:bg-my-black-700/85">
+        <figure
+          class="relative overflow-hidden rounded-2xl shadow-xl before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-transparent before:transition-all before:duration-200 before:ease-in-out before:content-[''] before:group-hover:bg-black/50"
+        >
+          <div
+            class="absolute left-1/2 top-1/2 z-10 flex h-14 w-14 -translate-x-1/2 -translate-y-1/2 scale-75 items-center justify-center rounded-2xl bg-white text-yellow-500 opacity-0 shadow-lg transition-all duration-200 ease-in-out group-hover:scale-100 group-hover:opacity-100 dark:bg-my-black-700/85"
+          >
             <i class="ri-external-link-line text-xl" />
           </div>
           <img
             :src="item.imageUrl"
             :alt="item.title"
             :style="{ 'object-position': 'center top' }"
-            class="group-hover:scale-110 h-full w-full object-cover transition-all duration-200 ease-in-out"
+            class="h-full w-full object-cover transition-all duration-200 ease-in-out group-hover:scale-110"
             loading="lazy"
           />
         </figure>
@@ -27,11 +31,18 @@
         <p class="text-base font-light text-my-black-300 dark:text-gray-300">
           {{ item.author }}
         </p>
-        <p class="mt-2 text-xs md:text-base text-my-black-300 dark:text-gray-300">
+        <p class="mt-2 text-xs text-my-black-300 md:text-base dark:text-gray-300">
           {{ item.description }}
         </p>
-        <div v-if="hasGenres" class="mt-2 md:mt-4 flex gap-2">
-          <BaseBadge v-for="genre in item.genres" :title="genre" />
+        <div
+          v-if="hasGenres"
+          class="mt-2 flex flex-wrap gap-2 md:mt-4"
+        >
+          <BaseBadge
+            v-for="(genre, index) in item.genres"
+            :title="genre"
+            :key="index"
+          />
         </div>
       </header>
     </div>
